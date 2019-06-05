5984c45fcbfdc9cf8a90ccd74a098f2d6a484f58
www <353485896@qq.com>
2019-06-05 19:35:04 +0800

    Initial commit

---
+++ module01/src/com/atguigu/Customer.java
@@ -0,0 +1,10 @@
+package com.atguigu;
+
+public class Customer {
+    public static void main(String[] args) {
+        System.out.println("module01");
+        for (String arg : args) {
+            
+        }
+    }
+}
